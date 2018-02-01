0f17b4e83a2124da9b91c072014d60c7f6b70acf
psych3 <j_shokoor@yahoo.com>
2018-01-31 19:04:03 -0800

    Create model for form input fields. Create FormContext and make necessary update to config and Startup file.

---
+++ iBot/Models/FormContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace iBot.Models
+{
+    public class FormContext : DbContext
+    {
+        public FormContext(DbContextOptions<FormContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Form> Form { get; set; }
+    }
+}
