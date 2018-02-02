95f71a9304f09bf10bac21ddc7ea5b1bdf5ff0d0
psych3 <j_shokoor@yahoo.com>
2018-02-01 19:51:52 -0800

    Parse and format pertinent values from 2nd row of output, on the ca.gov website.
    The form on the website returns a table of data, 2nd row of data has
    been parsed and formatted.

--- index.js
+++ index.js
@@ -3,14 +3,43 @@ var request = require('request');
 request.post({
     url: 'https://www.dir.ca.gov/dwc/pharmfeesched/pfs.asp',
     form: {
-        NDCno: '00002026002',
-        MDUnits: '123',
-        PriceBilled: '$12.45',
-        DateOfService: '1/5/2004'
+        NDCno: '55111068405',
+        MDUnits: '50',
+        PriceBilled: '$13.12',
+        DateOfService: '01/31/2018'
     }
-},
+    },
     function (err, httpResponse, body) {
 
-        console.log(httpResponse);
+        var myLongString = httpResponse.body;
+        var myTables = myLongString.split("class=\"tabborder\"");
+        var myRows = [];
+        myRows = myTables[1].split("<tr");
+
+        var itemsInSecondRow = myRows[2].split("<td>");
+        var itemToExtractFromSecondRow;
+        var temp;
+        var secondaryTemp = [];
+        // 0 to 6
+        for (var x = 0; x < itemsInSecondRow.length; x++)
+        {
+            console.log(x);
+            temp = itemsInSecondRow[x].split("\r\n");
+            console.log(typeof(temp[1]));
+            secondaryTemp.push(temp[1]);
+            
+        }
+
+        var thirdTemp;
+        var arrayOfCleanValues = [];
+        for (var x = 1; x < secondaryTemp.length; x++)
+        {
+            thirdTemp = secondaryTemp[x].trim();
+            console.log(thirdTemp);
+            arrayOfCleanValues.push(thirdTemp);
+        }
+        
+        console.log(secondaryTemp);
+        console.log(arrayOfCleanValues);
 
     })
