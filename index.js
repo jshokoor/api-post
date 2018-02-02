6574cc318b842ed2ad5ccaf0af0f21ba17aa0c7e
psych3 <j_shokoor@yahoo.com>
2018-02-01 23:54:20 -0800

    Parse all remaining values in output.
    All values from the output table have been parsed and populated into an
    array. Committing now before moving onto the hosting phase, using
    Express and ejs.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+
 var request = require('request');
+
+var $ = require('jquery');
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ posts: [], user: {} })
+    .write()
+
+// vars that end in 'AsInt' should be changed to 'AsFloat'
+var usualAndCustomaryPriceAsInt = 13.12;
+var usualAndCustomaryPrice = "$13.12"
+var totalOfIngredientsAsInt;
+var totalOfIngredients;
+var mediCalDispensingFeeAsInt = 7.25;
+var mediCalDispensingFee = "$7.25";
+var subtotalAsInt;
+var subtotal;
+var finalPrice;
 
 request.post({
     url: 'https://www.dir.ca.gov/dwc/pharmfeesched/pfs.asp',
     form: {
         NDCno: '55111068405',
         MDUnits: '50',
-        PriceBilled: '$13.12',
+        PriceBilled: usualAndCustomaryPrice,
         DateOfService: '01/31/2018'
     }
     },
@@ -15,11 +38,11 @@ request.post({
         var myTables = myLongString.split("class=\"tabborder\"");
         var myRows = [];
         myRows = myTables[1].split("<tr");
-
         var itemsInSecondRow = myRows[2].split("<td>");
         var itemToExtractFromSecondRow;
         var temp;
         var secondaryTemp = [];
+        
         // 0 to 6
         for (var x = 0; x < itemsInSecondRow.length; x++)
         {
@@ -35,11 +58,33 @@ request.post({
         for (var x = 1; x < secondaryTemp.length; x++)
         {
             thirdTemp = secondaryTemp[x].trim();
-            console.log(thirdTemp);
             arrayOfCleanValues.push(thirdTemp);
         }
         
-        console.log(secondaryTemp);
+        totalOfIngredientsAsInt = parseFloat(arrayOfCleanValues[arrayOfCleanValues.length - 1]);
+        totalOfIngredients = "$" + arrayOfCleanValues[arrayOfCleanValues.length - 1];
+        arrayOfCleanValues.push(totalOfIngredients);
+        arrayOfCleanValues.push(mediCalDispensingFee);
+        subtotalAsInt = totalOfIngredientsAsInt + mediCalDispensingFeeAsInt;
+        subtotal = subtotalAsInt.toString();
+        subtotal = "$" + subtotal;
+        arrayOfCleanValues.push(subtotal);
+        arrayOfCleanValues.push(usualAndCustomaryPrice);
+        if (usualAndCustomaryPriceAsInt < subtotalAsInt)
+        {
+            finalPrice = usualAndCustomaryPriceAsInt;
+        }
+        else
+        {
+            finalPrice = subtotal;
+        }
+        arrayOfCleanValues.push(finalPrice);
+        
         console.log(arrayOfCleanValues);
 
+        db.get('posts')
+            .push({ output: arrayOfCleanValues })
+            .write()
     })
+
+//document.getElementById('firstBox').value = arrayOfCleanValues;
